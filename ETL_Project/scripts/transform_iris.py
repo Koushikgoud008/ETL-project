b64b03528d57564f6c250d7d24fd60a1c6479db3
Koushikgoud008 <koushikgoud008@gmail.com>
2025-12-08 15:33:10 +0530

    DAY1

---
+++ ETL_Project/scripts/transform_iris.py
@@ -0,0 +1,45 @@
+#importing 
+import os
+import seaborn as sns
+import pandas as pd
+from extract_iris import extract_data
+
+def tranform_data(raw_path):
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    staged_dir = os.path.join(base_dir, "data", "staged")
+
+    os.makedirs(staged_dir, exist_ok= True)
+    df = pd.read_csv(raw_path)
+
+    #1.Handle Missing Values
+
+    numeric_cols = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    
+    #filling the missing values with median
+    for col in numeric_cols:
+        df[col] = df[col].fillna(df[col].median())
+    
+    df["species"] = df["species"].fillna(df["species"].mode()[0])
+
+    #2.Feature engineering
+
+    df["sepal_ratio"] = df["sepal_length"] / df["sepal_width"]
+    df["petal_ratio"] = df["petal_length"] / df["petal_width"]
+
+    df["is_petal_long"] = (df["petal_length"] > df["petal_length"].median()).astype(int)
+
+    #3.Drop unnecessary columns
+
+    df.drop(columns=[], inplace = True, errors = "ignore")
+    
+    #4. saved Data
+    staged_path = os.path.join(staged_dir, "clean_iris.csv")
+    df.to_csv(staged_path, index = False)
+
+    print(f"Data Transformed and Saved at : {staged_path}")
+    return staged_path
+
+if __name__ == "__main__":
+    from extract_iris import extract_data
+    raw_path = extract_data()
+    tranform_data(raw_path)
